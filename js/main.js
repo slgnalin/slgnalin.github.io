a0e4d44d202c3ba9a6adf09cf657ed1857c226ec
Salagean Alin <slgnalin@yahoo.com>
2021-01-29 23:08:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// hide navbar on scroll down and reveal on scroll up
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-bar").style.animation = "expand 1s forwards";
+    } else {
+        document.getElementById("nav-bar").style.animation = "retract 1s forwards";
+
+    }
+    prevScrollpos = currentScrollPos;
+}
